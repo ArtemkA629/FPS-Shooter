9edc4f746d1ea5537cd6cbbfd3efb80c4764bfab
ArtemkA629 <deffing9@mail.ru>
2023-06-12 09:29:57 +0300

    добавлен прицел и стрельба сделана по центру

--- Assets/Scripts/Gun.cs
+++ Assets/Scripts/Gun.cs
@@ -6,7 +6,9 @@ public class Gun : MonoBehaviour
     [SerializeField] private StarterAssetsInputs _input;
     [SerializeField] private GameObject _bulletPrefab;
     [SerializeField] private Transform _bulletPoint;
-    [SerializeField] private float _bulletSpeed = 600f;
+    [SerializeField] private Camera _camera;
+    [SerializeField] private float _bulletSpeed = 1000f;
+    [SerializeField] private float _spread = 0f;
 
     private void Update()
     {
@@ -20,8 +22,28 @@ public class Gun : MonoBehaviour
     private void Shoot()
     {
         Debug.Log("shoot!");
-        GameObject bullet = Instantiate(_bulletPrefab, _bulletPoint.position, transform.rotation);
-        bullet.GetComponent<Rigidbody>().AddForce(transform.forward * _bulletSpeed);
+
+        Ray ray = _camera.ViewportPointToRay(new Vector3(0.5f, 0.5f, 0f));
+        RaycastHit hit;
+
+        Vector3 targetPoint;
+        if (Physics.Raycast(ray, out hit))
+            targetPoint = hit.point;
+        else
+        {
+            float distance = 75;
+            targetPoint = ray.GetPoint(distance);
+        }
+        Vector3 dirWithoutSpread = targetPoint - _bulletPoint.position;
+
+        float x = Random.Range(-_spread, _spread);
+        float y = Random.Range(-_spread, _spread);
+
+        Vector3 dirWithSpread = dirWithoutSpread + new Vector3(x, y, 0);
+        GameObject bullet = Instantiate(_bulletPrefab, _bulletPoint.position, Quaternion.identity);
+        bullet.transform.forward = dirWithSpread.normalized;
+        bullet.GetComponent<Rigidbody>().AddForce(dirWithSpread.normalized * _bulletSpeed, ForceMode.Impulse);
+
         Destroy(bullet, 1);
     }
 }
